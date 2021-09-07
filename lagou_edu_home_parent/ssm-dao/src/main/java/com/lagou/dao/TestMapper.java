832c9407bd3985d69c0a7a013bd615a560bcc7b7
zhangpeng <260632858@qq.com>
2021-09-07 09:45:49 +0800

    第2次提交

---
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/TestMapper.java
@@ -0,0 +1,15 @@
+package com.lagou.dao;
+
+import com.lagou.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+    /*
+        对test表进行查询所有
+     */
+    public List<Test> findAllTest();
+
+
+}
---
+++ lagou_edu_home_parent/ssm-service/src/main/java/com/lagou/service/CourseService.java
@@ -0,0 +1,39 @@
+package com.lagou.service;
+
+import com.lagou.domain.Course;
+import com.lagou.domain.CourseVO;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public interface CourseService {
+
+    /*
+        多条件课程列表查询
+     */
+
+    public List<Course> findCourseByCondition(CourseVO courseVO);
+
+
+    /*
+        添加课程及讲师信息
+     */
+    public void saveCourseOrTeacher(CourseVO courseVO) throws InvocationTargetException, IllegalAccessException;
+
+    /*
+        根据ID查询课程信息
+     */
+    public CourseVO findCourseById(Integer id);
+
+    /*
+       更新课程及讲师信息
+     */
+    public void updateCourseOrTeacher(CourseVO courseVO) throws InvocationTargetException, IllegalAccessException;
+
+    /*
+        课程状态变更
+     */
+    public void updateCourseStatus(int courseid,int status);
+
+
+}
